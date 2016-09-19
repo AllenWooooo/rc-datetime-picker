c73c9e69b442f6967f9882869a36c1920af2ffb6
allenwu <mangong33@gmail.com>
2016-09-19 16:51:53 +0800

    [build] 1.0.0

---
+++ build/release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+echo "Enter release version: "
+read -r VERSION
+
+read -p "Releasing ${VERSION} - are you sure? (y/n)" -n 1 -r
+echo
+if [[ "${REPLY}" =~ ^[Yy]$ ]]; then
+    echo "Releasing ${VERSION}..."
+
+    # build
+    VERSION=${VERSION} npm run build
+    git add -A
+    git commit -m "[build] ${VERSION}" || :
+
+    # release
+    npm version "${VERSION}" --message "[release] ${VERSION}"
+
+    # publish
+    git push origin "v${VERSION}"
+    git push
+    npm publish --registry=https://registry.npmjs.org
+fi
