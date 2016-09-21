2ef5ef22b7b3897e00dcb91388c764661f97115a
allenwu <mangong33@gmail.com>
2016-09-21 18:29:50 +0800

    Add month panel

---
+++ src/panels/Month.jsx
@@ -0,0 +1,88 @@
+import React, {Component} from 'react';
+import moment from 'moment';
+import classNames from 'classnames/bind';
+
+import {MONTHS} from '../contants';
+import {chunk} from '../utils';
+
+
+class Month extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      moment: props.moment.clone(),
+      selected: props.moment.clone()
+    };
+  }
+
+  changeYear = (dir) => {
+    this.setState({
+      moment: this.state.moment[dir === 'prev' ? 'subtract' : 'add'](1, 'year')
+    });
+  }
+
+  select = (month, isSelected) => {
+    if (isSelected) return;
+
+    const _moment = this.state.moment;
+
+    _moment.month(month);
+
+    this.setState({
+      moment: _moment.clone(),
+      selected: _moment.clone()
+    });
+    this.props.onChange && this.props.onChange(_moment);
+  }
+
+  _renderMonth(month) {
+    const now = moment();
+    const _month = moment().month(month).month();
+    const _moment = this.state.moment;
+    const {selected} = this.state;
+    const isSelected = _moment.isSame(selected, 'year') && selected.month() === _month;
+    const className = classNames({
+      'selected': isSelected,
+      'now': _moment.isSame(now, 'year') && now.month() === _month
+    });
+
+    return (
+      <td key={month} className={className} onClick={() => this.select(_month, isSelected)}>{month}</td>
+    );
+  }
+
+  render() {
+    const _moment = this.state.moment;
+    const months = MONTHS;
+
+    return (
+      <div className="calendar-months">
+        <div className="calendar-nav">
+          <button type="button" className="prev-month" onClick={() => this.changeYear('prev')}>
+            <i className="fa fa-angle-left"/>
+          </button>
+          <span className="current-date">{_moment.format('YYYY')}</span>
+          <button type="button" className="next-month" onClick={() => this.changeYear('next')}>
+            <i className="fa fa-angle-right"/>
+          </button>
+        </div>
+        <table>
+          <tbody>
+            {chunk(months, 3).map((_months, idx) => {
+              return (
+                <tr key={idx}>
+                  {_months.map((month) => {
+                    return this._renderMonth(month);
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+
+export default Month;
