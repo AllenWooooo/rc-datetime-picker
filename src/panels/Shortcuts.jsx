8aab2356cbd928beb46559249fd8d234b86e27e4
AllenWooooo <mangong33@gmail.com>
2017-04-20 16:29:09 +0800

    Fix custome shortcut error.

--- src/panels/Shortcuts.jsx
+++ src/panels/Shortcuts.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import moment from 'moment';
 import classNames from 'classnames/bind';
 import {CUSTOME_BUTTON_TEXT} from '../constants';
 
@@ -7,19 +8,24 @@ const isSameRange = (current, value) => {
 }
 
 class Shortcuts extends Component {
-  handleClick = (value) => {
-    const {onChange} = this.props;
+  handleClick = (value, isCustome) => {
+    const {onChange, range, showCustomeButton} = this.props;
 
-    onChange && onChange(value, 'day');
+    if (range) {
+      onChange && onChange(value, isCustome);
+    } else {
+      onChange && onChange(value, 'day');
+    }
   }
 
   _renderShortcut = (key, value) => {
-    const {moment, range, shortcuts, customeButtonText = CUSTOME_BUTTON_TEXT} = this.props;
+    const {range, shortcuts, customeButtonText = CUSTOME_BUTTON_TEXT} = this.props;
+    const current = this.props.moment;
     const selected = range 
-                      ? key !== 'custome' && isSameRange(moment, value)
+                      ? key !== 'custome' && isSameRange(current, value)
                       : false;
     const isCustomeSelected = range
-                       ? !Object.keys(shortcuts).some((_key) => isSameRange(moment, shortcuts[_key])) && key === 'custome'
+                       ? !Object.keys(shortcuts).some((_key) => isSameRange(current, shortcuts[_key])) && key === 'custome'
                        : false;
     const className = classNames('btn', {
       selected: selected || isCustomeSelected
@@ -30,7 +36,7 @@ class Shortcuts extends Component {
         className={className} 
         key={key} 
         type="button" 
-        onClick={this.handleClick.bind(this, value)}>
+        onClick={this.handleClick.bind(this, value, key === 'custome')}>
         {key === 'custome' ? customeButtonText : key}
       </button>
     );
@@ -38,10 +44,17 @@ class Shortcuts extends Component {
 
   _renderShortcuts = () => {
     const {shortcuts, showCustomeButton} = this.props;
-    const renderShortcuts = showCustomeButton ? {...shortcuts, 'custome': {}} : shortcuts;
+    const renderShortcuts = showCustomeButton 
+                             ? {
+                                ...shortcuts, 
+                                custome: {
+                                  start: moment().subtract(29, 'days'),
+                                  end: moment().endOf('day')
+                                }} 
+                             : shortcuts;
 
     return Object.keys(renderShortcuts).map((key) => {
-      return this._renderShortcut(key, shortcuts[key]);
+      return this._renderShortcut(key, renderShortcuts[key]);
     });
   }
 
