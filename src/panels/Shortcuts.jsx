d46c07f5b7cb6d17ddaecbc0ce2178fdb5160981
AllenWooooo <mangong33@gmail.com>
2017-04-20 11:39:34 +0800

    Activate shortcut when selected

--- src/panels/Shortcuts.jsx
+++ src/panels/Shortcuts.jsx
@@ -1,5 +1,10 @@
 import React, {Component} from 'react';
+import classNames from 'classnames/bind';
+import {CUSTOME_BUTTON_TEXT} from '../constants';
 
+const isSameRange = (current, value) => {
+  return current.start && current.end && current.start.isSame(value.start, 'day') && current.end.isSame(value.end, 'day')
+}
 
 class Shortcuts extends Component {
   handleClick = (value) => {
@@ -9,15 +14,33 @@ class Shortcuts extends Component {
   }
 
   _renderShortcut = (key, value) => {
+    const {moment, range, shortcuts, customeButtonText = CUSTOME_BUTTON_TEXT} = this.props;
+    const selected = range 
+                      ? key !== 'custome' && isSameRange(moment, value)
+                      : false;
+    const isCustomeSelected = range
+                       ? !Object.keys(shortcuts).some((_key) => isSameRange(moment, shortcuts[_key])) && key === 'custome'
+                       : false;
+    const className = classNames('btn', {
+      selected: selected || isCustomeSelected
+    });
+
     return (
-      <button className="btn" key={key} type="button" onClick={this.handleClick.bind(this, value)}>{key}</button>
+      <button 
+        className={className} 
+        key={key} 
+        type="button" 
+        onClick={this.handleClick.bind(this, value)}>
+        {key === 'custome' ? customeButtonText : key}
+      </button>
     );
   }
 
   _renderShortcuts = () => {
-    const {shortcuts} = this.props;
+    const {shortcuts, showCustomeButton} = this.props;
+    const renderShortcuts = showCustomeButton ? Object.assign(shortcuts, {'custome': {}}) : shortcuts;
 
-    return Object.keys(shortcuts).map((key) => {
+    return Object.keys(renderShortcuts).map((key) => {
       return this._renderShortcut(key, shortcuts[key]);
     });
   }
